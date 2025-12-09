b01d067f28ea89fbce52964d18f8495387ec1f7b
Moses Kim <sangonkim1998@gmail.com>
2025-12-09 18:23:03 +1100

    fixed prisma

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,16 +1,24 @@
 // $lib/server/prisma.ts
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+
+const { PrismaClient } = pkg;
 
 const connectionString = process.env.DATABASE_URL!;
 
 const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = globalThis as unknown as {
-	prisma: PrismaClient | undefined;
+    prisma: InstanceType<typeof PrismaClient> | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        adapter
+    });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
